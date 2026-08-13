f574d168287c7a5b3549aa4e090cca2cbb0bc038
Samuel Williams <samuel.williams@shopify.com>
2026-08-13 20:59:51 +1200

    Rely on gRPC header policy
    Assisted-By: devx/48bd9802-2aca-41ba-856d-0b5bd523f9cf

--- test/protocol/grpc/metadata.rb
+++ test/protocol/grpc/metadata.rb
@@ -8,20 +8,6 @@ require "protocol/grpc/status"
 require "protocol/http"
 
 describe Protocol::GRPC::Metadata do
-	def raw_headers(values)
-		Class.new do
-			attr_accessor :policy
-			
-			define_method(:initialize) do |values|
-				@values = values
-			end
-			
-			define_method(:[]) do |key|
-				@values[key]
-			end
-		end.new(values)
-	end
-	
 	with ".extract_status" do
 		it "extracts status from headers" do
 			headers = Protocol::HTTP::Headers.new([%w[grpc-status 0]], nil, policy: Protocol::GRPC::HEADER_POLICY)
@@ -34,51 +20,20 @@ describe Protocol::GRPC::Metadata do
 		end
 		
 		it "extracts non-zero status" do
-			headers = Protocol::HTTP::Headers.new([%w[grpc-status 14]], nil, policy: Protocol::GRPC::HEADER_POLICY)
+			headers = Protocol::HTTP::Headers.new([%w[grpc-status 14]])
 			expect(subject.extract_status(headers)).to be == Protocol::GRPC::Status::UNAVAILABLE
-		end
-		
-		it "extracts a raw scalar status" do
-			headers = raw_headers("grpc-status" => "14")
-			expect(subject.extract_status(headers)).to be == Protocol::GRPC::Status::UNAVAILABLE
-		end
-		
-		it "extracts a raw array status" do
-			headers = raw_headers("grpc-status" => [nil, "13"])
-			expect(subject.extract_status(headers)).to be == Protocol::GRPC::Status::INTERNAL
-		end
-		
-		it "extracts a deeply nested raw array status" do
-			status = Class.new(Array) do
-				def flatten
-					[[["5"]]]
-				end
-			end.new
-			headers = raw_headers("grpc-status" => status)
-			
-			expect(subject.extract_status(headers)).to be == Protocol::GRPC::Status::NOT_FOUND
 		end
 	end
 	
 	with ".extract_message" do
 		it "extracts message from headers" do
-			headers = Protocol::HTTP::Headers.new([["grpc-message", "Error%20message"]], nil, policy: Protocol::GRPC::HEADER_POLICY)
+			headers = Protocol::HTTP::Headers.new([["grpc-message", "Error%20message"]])
 			expect(subject.extract_message(headers)).to be == "Error message"
 		end
 		
 		it "returns nil if message not present" do
 			headers = Protocol::HTTP::Headers.new
 			expect(subject.extract_message(headers)).to be_nil
-		end
-		
-		it "extracts a raw scalar message" do
-			headers = raw_headers("grpc-message" => "Error%20message")
-			expect(subject.extract_message(headers)).to be == "Error message"
-		end
-		
-		it "extracts a raw array message" do
-			headers = raw_headers("grpc-message" => ["Error%20message"])
-			expect(subject.extract_message(headers)).to be == "Error message"
 		end
 	end
 	
